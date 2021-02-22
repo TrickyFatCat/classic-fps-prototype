3259fbf682ed62ed29ff1ca595274319b861c467
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-02-22 23:12:00 +0300

    Adjust player camera and add camera controls

--- src/entity/player/player.gd
+++ src/entity/player/player.gd
@@ -1,3 +1,20 @@
 class_name Player
 extends Entity
 
+const CAM_ROT_MIN : float = -90.0
+const CAM_ROT_MAX : float = 90.0
+
+export var mouse_sens : float = 0.1
+
+onready var camera : Camera = $Camera
+
+
+func _init() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		camera.rotation_degrees.x -= mouse_sens * event.get_relative().x
+		camera.rotation_degrees.y -= mouse_sens * event.get_relative().y
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, CAM_ROT_MIN, CAM_ROT_MAX)
