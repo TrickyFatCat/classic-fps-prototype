89f70d6490221cacfe3aa711796ec31ff2422bd7
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-02-25 23:27:10 +0300

    Implement jump and adjust player speed

--- src/entity/player/player.gd
+++ src/entity/player/player.gd
@@ -9,11 +9,13 @@ var _player_direction : Vector3 = Vector3.ZERO
 var drag : float = 0.0
 var velocity : Vector3 = Vector3.ZERO
 var snap_vec : Vector3
+var is_jumping = false
 
 export var acceleration : float = 4.0
 export var speed_max : float = 25.0
-export var mouse_sens : float = 0.1
+export var jump_force : float = 25.0
 export var gravity : float = 60.0
+export var mouse_sens : float = 0.1
 export var ignore_rotation : bool = false
 
 onready var camera : Camera = $Camera
@@ -26,6 +28,8 @@ func _init() -> void:
 
 func _input(event: InputEvent) -> void:
 	_rotate_camera(event)
+	if event.is_action_pressed("jump"):
+		_apply_jump()
 
 
 func _process(delta: float) -> void:
@@ -33,15 +37,16 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	var result_direction : Vector3 = _move_direction
+	var current_move_direction : Vector3 = _move_direction
+	
+	if _move_direction == Vector3.ZERO and (abs(velocity.x) != 0 or abs(velocity.z) != 0):
+		current_move_direction = velocity.normalized()
 	
 	if not ignore_rotation:
-		result_direction = result_direction.rotated(Vector3.UP, rotation.y)
-	
+		current_move_direction = current_move_direction.rotated(Vector3.UP, rotation.y)
 	var drag_vec : Vector3 = Vector3(drag, 0, drag)
-	velocity = acceleration * result_direction - velocity * drag_vec + gravity * Vector3.DOWN * delta
+	velocity += acceleration * current_move_direction - velocity * drag_vec + gravity * Vector3.DOWN * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
-	pass
 
 
 func _rotate_camera(event: InputEvent) -> void:
@@ -54,14 +59,8 @@ func _rotate_camera(event: InputEvent) -> void:
 func _calculate_move_direction() -> void:
 	_move_direction.x = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
 	_move_direction.z = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
-	#if Input.is_action_pressed("move_forward"):
-	#	_move_direction += Vector3.FORWARD
-	#elif Input.is_action_pressed("move_backward"):
-	#	_move_direction += Vector3.BACK
-	#elif Input.is_action_pressed("move_left"):
-	#	_move_direction += Vector3.LEFT
-	#elif Input.is_action_pressed("move_right"):
-	#	_move_direction += Vector3.RIGHT
-	
 	_move_direction = _move_direction.normalized()
-	
+
+func _apply_jump() -> void:
+	if is_on_floor() :
+		velocity.y = jump_force
