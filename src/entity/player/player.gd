50c0d481d5313ea50a7ec4f29809a25f4a109c58
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-02-27 23:44:07 +0300

    Rework movement calculations

--- src/entity/player/player.gd
+++ src/entity/player/player.gd
@@ -6,13 +6,14 @@ const CAM_ROT_MAX : float = 90.0
 
 var _move_direction : Vector3 = Vector3.ZERO
 var _player_direction : Vector3 = Vector3.ZERO
-var drag : float = 0.0
 var velocity : Vector3 = Vector3.ZERO
 var snap_vec : Vector3
 var is_jumping = false
 
-export var acceleration : float = 4.0
+export var acceleration : float = 250.0
 export var speed_max : float = 25.0
+export var ground_friction : float = 100.0
+export var air_friction : float = 25.0
 export var jump_force : float = 25.0
 export var gravity : float = 60.0
 export var mouse_sens : float = 0.1
@@ -23,7 +24,6 @@ onready var camera : Camera = $Camera
 
 func _init() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	drag = acceleration / speed_max
 
 
 func _input(event: InputEvent) -> void:
@@ -37,18 +37,16 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	var current_move_direction : Vector3 = _move_direction
-	
-	#if _move_direction == Vector3.ZERO and (abs(velocity.x) != 0 or abs(velocity.z) != 0):
-		#current_move_direction = velocity.normalized()
-	
+	var current_direction = _move_direction
 	if not ignore_rotation:
-		current_move_direction = current_move_direction.rotated(Vector3.UP, rotation.y)
-	var drag_vec : Vector3 = Vector3(drag, 0, drag)
-	velocity += acceleration * current_move_direction - velocity * drag_vec + gravity * Vector3.DOWN * delta
-	velocity.x = clamp(velocity.x, -speed_max, speed_max)
-	velocity.z = clamp(velocity.z, -speed_max, speed_max)
+		_move_direction = _move_direction.rotated(Vector3.UP, rotation.y)
+	
+	_calculate_velocity_x(_move_direction)
+	_calculate_velocity_z(_move_direction)
+	_apply_gravity()
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	$Label.text = String(velocity)
 
 
 func _rotate_camera(event: InputEvent) -> void:
@@ -59,9 +57,44 @@ func _rotate_camera(event: InputEvent) -> void:
 
 
 func _calculate_move_direction() -> void:
-	_move_direction.x = Input.get_action_strength("move_right") + Input.get_action_strength("move_left")
+	_move_direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	_move_direction.z = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
 	_move_direction = _move_direction.normalized()
+
+
+func _calculate_velocity_x(direction: Vector3) -> void:
+	if direction.x != 0 and abs(velocity.x) <= speed_max:
+		velocity.x += acceleration * direction.x * get_physics_process_delta_time()
+		velocity.x = clamp(velocity.x, -speed_max, speed_max)
+	elif velocity.x != 0 or abs(velocity.x) > speed_max:
+		direction.x = -sign(velocity.x)
+		var friction = ground_friction if is_on_floor() else air_friction
+		velocity.x += friction * direction.x * get_physics_process_delta_time()
+		
+		if direction.x < 0:
+			velocity.x = max(velocity.x, 0)
+		elif direction.x > 0:
+			velocity.x = min(velocity.x, 0)
+
+
+func _calculate_velocity_z(direction: Vector3) -> void:
+	if direction.z != 0 and abs(velocity.z) <= speed_max:
+		velocity.z += acceleration * direction.z * get_physics_process_delta_time()
+		velocity.z = clamp(velocity.z, -speed_max, speed_max)
+	elif velocity.z != 0 or abs(velocity.z) > speed_max:
+		direction.z = -sign(velocity.z)
+		var friction = ground_friction if is_on_floor() else air_friction
+		velocity.z += friction * direction.z * get_physics_process_delta_time()
+		
+		if direction.z < 0:
+			velocity.z = max(velocity.z, 0)
+		elif direction.z > 0:
+			velocity.z = min(velocity.z, 0)
+
+
+func _apply_gravity() -> void:
+	velocity.y -= gravity * get_physics_process_delta_time()
+
 
 func _apply_jump() -> void:
 	if is_on_floor() :
