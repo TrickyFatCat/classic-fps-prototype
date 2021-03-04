1fd719bdbaf291c19d9bb91298ab173629bab293
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-03-04 22:13:35 +0300

    Rework gravity applying logic for Player

--- src/entity/player/player.gd
+++ src/entity/player/player.gd
@@ -3,19 +3,23 @@ extends Entity
 
 const CAM_ROT_MIN : float = -90.0
 const CAM_ROT_MAX : float = 90.0
+const GROUND_CHECK_DISTANCE : float = 0.25
+const GROUND_COLLISION_MASK : int = 1
 
 var _move_direction : Vector3 = Vector3.ZERO
 var _player_direction : Vector3 = Vector3.ZERO
 var _velocity_horizontal : Vector3 = Vector3.ZERO
 var _velocity_vertical : Vector3 = Vector3.ZERO
+var _is_on_ground : bool = true
+
 var health_node : BaseResource
 
 export var acceleration : float = 15.0
-export var speed_max : float = 25.0
+export var speed_max : float = 20.0
 export var ground_friction : float = 12.0
-export var air_friction : float = 1.5
-export var jump_force : float = 25.0
-export var gravity : float = 60.0
+export var air_friction : float = 3.0
+export var jump_force : float = 20.0
+export var gravity : float = 98.0
 export var air_control_factor : float = 0.1
 export var mouse_sens : float = 0.1
 export var ignore_rotation : bool = false
@@ -26,6 +30,7 @@ onready var camera : Camera = $Camera
 
 func _init() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 	health_node = BaseResource.new(health_max, health_max);
 	health_node.name = "HealthPoints"
 	add_child(health_node);
@@ -34,6 +39,7 @@ func _init() -> void:
 func _input(event: InputEvent) -> void:
 	_rotate_camera(event)
 	if event.is_action_pressed("jump"):
+		return
 		_apply_jump()
 
 
@@ -42,11 +48,15 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
+	var ground_collision : Dictionary = _get_ground_collision()
+	_is_on_ground = not ground_collision.empty()
 	_move_direction = _move_direction.rotated(Vector3.UP, rotation.y)
 	_calculate_horizontal_velocity(_move_direction)
-	_apply_gravity()
+	_calculate_vertical_velocity()
+	_apply_jump()
 	var velocity = _velocity_horizontal + _velocity_vertical
-	move_and_slide(velocity, Vector3.UP)
+	move_and_slide(velocity, Vector3.UP, true, 4, 1)
+	$Label.text = String(velocity)
 
 
 func _rotate_camera(event: InputEvent) -> void:
@@ -73,13 +83,22 @@ func _calculate_horizontal_velocity(direction: Vector3) -> void:
 	_velocity_horizontal = _velocity_horizontal.linear_interpolate(direction * speed_max, velocity_factor * get_physics_process_delta_time())
 
 
-func _apply_gravity() -> void:
-	if is_on_floor():
-		return
-	
-	_velocity_vertical.y -= gravity * get_physics_process_delta_time()
+func _calculate_vertical_velocity() -> void:
+	if not is_on_floor():
+		_velocity_vertical += Vector3.DOWN * gravity * get_physics_process_delta_time()
+	elif is_on_floor() and _is_on_ground:
+		_velocity_vertical = -get_floor_normal() * gravity
+	else:
+		_velocity_vertical = -get_floor_normal()
 
 
 func _apply_jump() -> void:
-	if is_on_floor() :
-		_velocity_vertical.y = jump_force
+	if (is_on_floor() or _is_on_ground) and Input.is_action_just_pressed("jump"):
+		_velocity_vertical = Vector3.UP * jump_force
+
+
+func _get_ground_collision() -> Dictionary:
+	var space_state = get_world().direct_space_state
+	var start_point : Vector3 = global_transform.origin
+	var end_point : Vector3 = start_point + Vector3.DOWN * GROUND_CHECK_DISTANCE
+	return space_state.intersect_ray(start_point, end_point, [self], GROUND_COLLISION_MASK)
